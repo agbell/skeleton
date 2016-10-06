829af1d4c5e862d6f34c9780bfb829769162b2ba
Adam Bell <agbell@gmail.com>
2016-10-06 21:59:39 +0000

    SAVEPOINT

--- src/test/scala/com/cascadeofinsights/skeleton/AppTest.scala
+++ src/test/scala/com/cascadeofinsights/skeleton/AppTest.scala
@@ -1,24 +1,42 @@
-package com.cascadeofinsights.skeleton
-
-import org.scalatest._
-
+import org.scalatest.FunSpec
+import org.scalatest.GivenWhenThen
 import scala.collection.mutable.Stack
 
-class AppTest extends FlatSpec with Matchers {
+class StackSpec extends FunSpec with GivenWhenThen {
 
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be(2)
-    stack.pop() should be(1)
-  }
+  describe("A Stack") {
 
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a[NoSuchElementException] should be thrownBy {
-      emptyStack.pop()
+    it("should pop values in last-in-first-out-order") {
+
+      Given("a non-empty stack")
+      val stack = new Stack[Int]
+      stack.push(1)
+      stack.push(2)
+      val oldSize = stack.size
+
+      When("pop is invoked on the stack")
+      val result = stack.pop()
+
+      Then("the most recently pushed element should be returned")
+      assert(result === 2)
+
+      And("the stack should have one less item than before")
+      assert(stack.size === oldSize - 1)
+    }
+
+    it("should throw NoSuchElementException if an empty stack is popped") {
+
+      Given("an empty stack")
+      val emptyStack = new Stack[String]
+
+      When("pop is invoked on the stack")
+      When("NoSuchElementException should be thrown")
+      intercept[NoSuchElementException] {
+        emptyStack.pop()
+      }
+
+      And("the stack should still be empty")
+      assert(emptyStack.isEmpty)
     }
   }
-
 }
